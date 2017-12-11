ad1f5a4a727dfb6c8e8c0911ed94032625643967
Howard Yeh <howard@metacircus.com>
2017-12-11 14:36:38 +0800

    add loopring contracts

---
+++ contracts/test/DummyToken.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.4.19;
+
+import "zeppelin-solidity/contracts/token/MintableToken.sol";
+
+
+contract DummyToken is MintableToken {
+    string public name;
+    string public symbol;
+    uint8  public decimals;
+
+    function DummyToken(
+        string _name,
+        string _symbol,
+        uint8  _decimals,
+        uint   _totalSupply
+        )
+        public
+    {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
+    }
+
+    function setBalance(
+        address _target,
+        uint _value
+        )
+        onlyOwner
+        public
+    {
+        uint currBalance = balanceOf(_target);
+        if (_value < currBalance) {
+            totalSupply = totalSupply.sub(currBalance.sub(_value));
+        } else {
+            totalSupply = totalSupply.add(_value.sub(currBalance));
+        }
+        balances[_target] = _value;
+    }
+}
