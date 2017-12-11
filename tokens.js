3d9ca475a7f18c3aa7af81c93a5cdd58ac79e70b
Howard Yeh <howard@metacircus.com>
2017-12-11 14:33:53 +0800

    tokens data

---
+++ tokens.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: [
+    {
+      decimals: 18,
+      name: "Loopring Coin",
+      symbol: "LRC",
+    },
+    {
+      decimals: 18,
+      name: "EOS",
+      symbol: "EOS",
+    },
+    {
+      decimals: 18,
+      name: "Augur Reputation Token",
+      symbol: "REP",
+    },
+    {
+      decimals: 18,
+      name: "NEO",
+      symbol: "NEO",
+    },
+    {
+      decimals: 18,
+      name: "QTUM",
+      symbol: "QTUM",
+    },
+  ],
+
+  live: [ ],
+};
